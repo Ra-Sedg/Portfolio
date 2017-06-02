be716ad76e4227eb8be320d1ca37432f5e49f5fe
Robert Sedgwick <Robert Sedgwick>
2017-06-02 10:38:26 -0400

    Weather Underground api implemented and working.

---
+++ Personal_Projects/WeatherZap/WeatherZap/Scripts/site.js
@@ -0,0 +1,62 @@
+﻿$(function () {
+
+    var zipcode;
+    console.log("site.js loaded");
+
+    navigator.permissions.query({ name: 'geolocation' })
+        .then(function (permissionStatus) {
+
+            console.log(permissionStatus.state);
+
+            /* geolocation is available */
+            if (permissionStatus.state == "granted" || permissionStatus.state == "prompt" ) {
+
+                navigator.geolocation.getCurrentPosition(function (position) {
+
+                    var lattitude = position.coords.latitude;
+                    var longitude = position.coords.longitude;
+
+                    $.getJSON("http://api.wunderground.com/api/8e2d1db7242415ea/geolookup/q/" +
+                        lattitude + "," + longitude + ".json", function () { })
+                        .done(function (data) {
+
+                            zipcode = data.location.zip;
+                            console.log(zipcode);
+                            getWeather(zipcode);
+
+                        });
+                });
+            }
+
+            $('#zip_input').keypress(function (e) {
+                if (e.which == 13) {
+                    zipcode = $("#zip_input").val();
+                    console.log(zipcode);
+                    getWeather(zipcode);
+                }
+            });
+
+        });
+
+    function getWeather(zipcode) {
+
+        $.get("http://api.wunderground.com/api/8e2d1db7242415ea/conditions/q/" +
+            zipcode + ".json", function () { })
+            .done(function (data) {
+
+                console.log(data);
+
+                $("#location").text("Location: " +
+                    data.current_observation.display_location.city + ", " +
+                    data.current_observation.display_location.state);
+
+                $("#temp_f").text("Tempurature: " + data.current_observation.temp_f +
+                    String.fromCharCode(176));
+
+                $("#forcast_img").attr("src", data.current_observation.icon_url);
+
+                $("#forcast_text").text(data.current_observation.icon);
+            });
+
+    }
+});
